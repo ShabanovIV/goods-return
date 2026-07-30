4b5545ce4ad2a3b16e2acf958c278f43f1ce973a
ShabanovIV <react.dev.shabanov@mail.ru>
2026-07-30 23:38:02 +0500

    test: cover request console route

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,7 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './store/store';
 
-test('mounts app root', () => {
-  render(<App />);
+test('renders the lazy request console route', async () => {
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/request-console']}>
+        <App />
+      </MemoryRouter>
+    </Provider>,
+  );
+
   expect(screen.getByTestId('app-root')).toBeInTheDocument();
+  expect(await screen.findByRole('button', { name: 'Выполнить запрос' })).toBeInTheDocument();
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+// eslint-disable-next-line import-x/no-nodejs-modules -- Jest needs the Node encoder polyfill.
+import { TextDecoder, TextEncoder } from 'util';
+
+Object.assign(globalThis, { TextDecoder, TextEncoder });
+Object.assign(globalThis, {
+  fetch: jest.fn(() => Promise.reject(new Error('Unexpected request in test'))),
+});
